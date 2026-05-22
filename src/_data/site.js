469819fe3d776b664910f46892105e0ef76b5c8d
Seyyed Ali Mohammadiyeh (Max Base) <MaxBaseCode@Gmail.Com>
2026-05-22 19:41:19 +0330

    Fix ai feedbacks

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,9 @@
 module.exports = {
   name: "NextCommunity Developer Directory",
+  author: "NextCommunity",
   url: "https://nextcommunity.github.io",
   locale: "en_US",
+  themeColor: "#2563eb",
   twitterCard: "summary_large_image",
   defaultImage: "/assets/img/next.jpeg",
 };
